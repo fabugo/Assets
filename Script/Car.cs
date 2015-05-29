e91ec2fc663c437ba7280a0c3fe6f078a78f6aa5
fabugo <fabugo@gmail.com>
2015-05-29 13:47:36 -0300

    construcao da simulacao

---
+++ Script/Car.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Car : MonoBehaviour {
+	public float speed;
+	public float speedTurn;
+	public bool canMove;
+	public transform destiny;
+	// Use this for initialization
+	void Start () {
+		canMove = true;
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (canMove)
+			transform.Translate(new Vector3(-1f * Time.deltaTime * speed,0f,0f));
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.CompareTag("Signal"))
+        	canMove = false;
+		if(other.gameObject.CompareTag("Car"))
+        	canMove = false;
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+		if(other.gameObject.CompareTag("Signal"))
+        	canMove = true;
+		if(other.gameObject.CompareTag("Car"))
+        	canMove = true;
+    }
+
+}
---
+++ Script/In.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class In : MonoBehaviour {
+	public Transform prefabCar;
+	private int countCar;
+	// Use this for initialization
+	void Start () {
+		countCar = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (countCar <= 5){
+			Instantiate (prefabCar,transform.position,Quaternion.identity);
+			countCar++;
+		}
+	}
+}
